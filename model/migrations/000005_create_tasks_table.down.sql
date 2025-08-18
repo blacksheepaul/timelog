2140025a863f37bc6daf9c0486510bff418dab50
jiyr <navidog@163.com>
2025-08-18 22:39:48 +0800

    chore: remove corrupted text in migration

--- model/migrations/000005_create_tasks_table.down.sql
+++ model/migrations/000005_create_tasks_table.down.sql
@@ -1,9 +1,9 @@
---  dû¡h„"
+-- Drop indexes for tasks table
 DROP INDEX IF EXISTS idx_tasks_deleted_at;
 DROP INDEX IF EXISTS idx_tasks_completed_at;
 DROP INDEX IF EXISTS idx_tasks_is_completed;
 DROP INDEX IF EXISTS idx_tasks_due_date;
 DROP INDEX IF EXISTS idx_tasks_tag_id;
 
---  dû¡h
+-- Drop tasks table
 DROP TABLE IF EXISTS tasks;
--- model/migrations/000006_add_task_id_to_timelogs.up.sql
+++ model/migrations/000006_add_task_id_to_timelogs.up.sql
@@ -1,9 +1,9 @@
--- timelogshû task_idWµå/û¡sT
+-- Add task_id column to timelogs table
 ALTER TABLE timelogs ADD COLUMN task_id INTEGER;
 
--- û .¦_
--- èSQLite/ô¥û .¦_ÙÌÅ\:‡cô
--- FOREIGN KEY (task_id) REFERENCES tasks(id)
+-- Note: SQLite doesn't support adding foreign key constraints to existing tables
+-- The foreign key constraint FOREIGN KEY (task_id) REFERENCES tasks(id) 
+-- is handled in the application code
 
--- ú"ÐØåâ'ý
+-- Create index for task_id column
 CREATE INDEX idx_timelogs_task_id ON timelogs(task_id);
